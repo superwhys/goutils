b7578c10bdbb67e20ce2bb9a0373c3c15301c5b0
superwhys <269085434@qq.com>
2024-01-28 16:20:39 +0800

    feat: add json body struct

---
+++ http/json.go
@@ -0,0 +1,31 @@
+package http
+
+import (
+	"encoding/json"
+
+	"github.com/superwhys/goutils/lg"
+)
+
+type JsonBody map[string]any
+
+func NewJsonBody() JsonBody {
+	return make(JsonBody)
+}
+
+func (j JsonBody) Add(key string, value any) JsonBody {
+	_, exists := j[key]
+	if !exists {
+		j[key] = value
+	}
+	return j
+}
+
+func (j JsonBody) Encode() []byte {
+	b, err := json.Marshal(j)
+	if err != nil {
+		lg.Errorf("json body encode error: %v", err)
+		return nil
+	}
+
+	return b
+}
