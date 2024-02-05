1fcd951896b9035312c3422aa451a74fe9f45738
superwhys <269085434@qq.com>
2024-02-05 11:52:12 +0800

    feat: add router group utils

--- ginutils/engine.go
+++ ginutils/engine.go
@@ -13,6 +13,10 @@ type Engine struct {
 	*gin.Engine
 }
 
+type RouterGroup struct {
+	*gin.RouterGroup
+}
+
 func New(middlewares ...gin.HandlerFunc) *Engine {
 	if !lg.IsDebug() {
 		gin.SetMode(gin.ReleaseMode)
@@ -26,24 +30,40 @@ func New(middlewares ...gin.HandlerFunc) *Engine {
 	return &Engine{engine}
 }
 
+func (e *Engine) Group(relativePath string, handlers ...gin.HandlerFunc) *RouterGroup {
+	return &RouterGroup{
+		e.Engine.Group(relativePath, handlers...),
+	}
+}
+
 func (e *Engine) RegisterRouter(ctx context.Context, method, path string, handler RouteHandler) {
 	e.Handle(method, path, func(c *gin.Context) {
-		if err := c.ShouldBind(handler); err != nil {
-			AbortWithError(c, http.StatusBadRequest, "parse request params error")
-			return
-		}
+		baseHandler(c, ctx, handler)
+	})
+}
 
-		returnData, statusCode, err := handler.HandleFunc(c)
-		if err != nil {
-			AbortWithError(c, statusCode, fmt.Sprintf("%v handler error", lg.StructName(handler)))
-			return
-		}
+func (rg *RouterGroup) RegisterRouter(ctx context.Context, method, path string, handler RouteHandler) {
+	rg.Handle(method, path, func(c *gin.Context) {
+		baseHandler(c, ctx, handler)
+	})
+}
 
-		lg.Debugc(ctx, "%v handle done, status code: %v, data: %v", lg.StructName(handler), statusCode, returnData)
+func baseHandler(c *gin.Context, ctx context.Context, handler RouteHandler) {
+	if err := c.ShouldBind(handler); err != nil {
+		AbortWithError(c, http.StatusBadRequest, "parse request params error")
+		return
+	}
 
-		ReturnWithStatus(c, statusCode, Ret{
-			Code: 0,
-			Data: returnData,
-		})
+	returnData, statusCode, err := handler.HandleFunc(c)
+	if err != nil {
+		AbortWithError(c, statusCode, fmt.Sprintf("%v handler error", lg.StructName(handler)))
+		return
+	}
+
+	lg.Debugc(ctx, "%v handle done, status code: %v, data: %v", lg.StructName(handler), statusCode, returnData)
+
+	ReturnWithStatus(c, statusCode, Ret{
+		Code: 0,
+		Data: returnData,
 	})
 }
