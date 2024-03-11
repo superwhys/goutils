cb5e8c46ff86f6c9d3886bf175281794164628a6
superwhys <269085434@qq.com>
2024-03-11 15:53:43 +0800

    feat: add ginutils example

---
+++ ginutils/example/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/superwhys/goutils/flags"
+	"github.com/superwhys/goutils/ginutils"
+	"github.com/superwhys/goutils/lg"
+	"github.com/superwhys/goutils/service"
+)
+
+type Params struct {
+	Page int `form:"page"`
+	Size int `form:"size"`
+}
+
+type TestHandler struct {
+	ginutils.DefaultHandler
+}
+
+func (h *TestHandler) GetRequestParams() any {
+	return &Params{}
+}
+
+func (h *TestHandler) HandleFunc(c *gin.Context) (data any, statusCode int, err error) {
+	params := h.GetParams()
+	lg.Info(lg.Jsonify(params))
+
+	return fmt.Sprintf("success: %v", lg.Jsonify(params)), 200, nil
+}
+
+func main() {
+	flags.Parse()
+
+	router := ginutils.New()
+	router.GET(context.Background(), "/api/test", &TestHandler{})
+
+	srv := service.NewSuperService(
+		service.WithHttpHandler("", router),
+	)
+
+	srv.ListenAndServer(8080)
+}
