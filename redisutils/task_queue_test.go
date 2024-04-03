408b4b701e34e5a0c5d5054ff541ce315650cbdb
superwhys <269085434@qq.com>
2024-04-03 16:16:36 +0800

    update: perfect the unit-test

--- redisutils/task_queue_test.go
+++ redisutils/task_queue_test.go
@@ -1,6 +1,7 @@
 package redisutils
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -9,24 +10,34 @@ import (
 	"github.com/superwhys/goutils/dialer"
 )
 
+var (
+	queue *TaskQueue
+)
+
 type TestObj struct {
 	Age int
 }
 
-func TestRedisTaskQueue(t *testing.T) {
-	queue := NewTaskQueue(
+func TestMain(m *testing.M) {
+	queue = NewTaskQueue(
 		dialer.DialRedisPool("localhost:6379", 10, 100),
 		"testQueue",
 		&TestObj{},
 		WithBucket(6),
 	)
+	m.Run()
+}
 
+func TestRedisTaskQueue(t *testing.T) {
 	go func() {
 		for i := 0; i < 100; i++ {
 			key := fmt.Sprintf("taskkey-%v", i)
 			task := &TestObj{Age: i}
 			bucket := rand.Intn(6)
-			queue.PushToBucket(key, task, bucket, true)
+			if err := queue.PushToBucket(key, task, bucket, true); err != nil {
+				t.Error("push to bucket err")
+				return
+			}
 		}
 
 		time.Sleep(time.Second * 3)
@@ -35,5 +46,22 @@ func TestRedisTaskQueue(t *testing.T) {
 
 	for task := range queue.IterTask() {
 		fmt.Printf("receive task: %#v\n", task.Payload.(*TestObj))
+	}
+}
+
+func TestRedisTaskQueueDup(t *testing.T) {
+	task := &TestObj{Age: 1}
+
+	if err := queue.PushToBucket("dupKey", task, 1, true); err != nil {
+		t.Error("push to bucket err")
+		return
+	}
+	if err := queue.PushToBucket("dupKey", task, 1, true); err != nil {
+		if !errors.Is(err, ErrDuplicated) {
+			t.Error("err is not dup")
+			return
+		}
+	} else {
+		t.Error("except a dup err")
 	}
 }
